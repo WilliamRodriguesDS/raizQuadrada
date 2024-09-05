b9a0ce0c07b93bb1c2050f386b2db76b3b01f09c
WilliamRodriguesDS <williamrodriguesdeoliveira22@gmail.com>
2024-09-05 06:29:10 -0300

    RaizQuadrada

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function Calcular(){
     let numero = prompt("Digite um número")
     numero = parseFloat(numero);
-    let resultado = Math.sqrt(numero)
+    let resultado = Math.sqrt(numero);
+    let output = document.querySelector("#output");
     output.innerHTML = "A raiz quadrada do " + numero + " = " + resultado;
 }
 function Limpar(){
